d60de6c39efcd3e454ab999bbbf5cb71319045e1
Aaron <houghtalinga1@gmail.com>
2020-10-13 13:10:18 -0400

    Update main.py
    Added some testing statements

--- main.py
+++ main.py
@@ -2,5 +2,16 @@ from openTicket import *
 
 input = input("Please describe your issue: ")
 
-t1 = Ticket(input)
+t1 = Ticket(input, "open")
 t1.displayIssue()
+print("State is:", t1.getState())
+
+t2 = Ticket("No more issues", "closed")
+t2.displayIssue()
+print("State is:", t2.getState())
+t2.setState("open")
+print("State changed to:", t2.getState())
+
+t1.displayIssue()
+
+t1.startTime()
