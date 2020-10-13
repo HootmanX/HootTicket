0a97d83fc6173569be7ac25b420e20f19e7d731c
Aaron <houghtalinga1@gmail.com>
2020-10-13 10:25:47 -0400

    Update openTicket.py
    Added State to ticket - track opened/closed ticket

--- openTicket.py
+++ openTicket.py
@@ -1,9 +1,12 @@
 import time
 
 class Ticket:
-        def __init__(self, issue):
+        def __init__(self, issue, status):
             self.issue = issue
             self.startTime = time.localtime() # potential issue with timezones
 
         def displayIssue(self):
             print(self.issue)
+        
+        def state(self, status):
+                self.status=status
