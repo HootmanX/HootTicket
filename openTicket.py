58408c1f06211f5e848ccc5ea1d998573cd9d96b
Aaron <houghtalinga1@gmail.com>
2020-10-14 17:42:34 -0400

    Update openTicket.py
    Uploaded the previous version, better readability

--- openTicket.py
+++ openTicket.py
@@ -1,29 +1,29 @@
 from datetime import datetime
 
+
 class Ticket:
-        def __init__(self, id, issue):
-            self.issue = issue
-            #self.state = state
-            self.id = id
-            # Grab time as start of ticket
-            self.now = datetime.now()
-            self.openTime = self.now.strftime("%m/%d/%Y, %H:%M:%S")
-            self.notes = []
+    def __init__(self, id, issue):
+        self.issue = issue
+        # self.state = state
+        # Grab time as start of ticket
+        self.now = datetime.now()
+        self.openTime = self.now.strftime("%m/%d/%Y, %H:%M:%S")
+        self.notes = []
 
-        def displayIssue(self):
-            print(self.issue)
-        
-        #def setState(self, state):
-        #        self.state=state
+    def displayIssue(self):
+        print(self.issue)
 
-        #def getState(self) :
-        #    return self.state
-            
-        def showTime(self) :
-            print(self.openTime)
-            
-        def addNotes(self, comment) :
-            self.notes.append(comment)
-            
-        def displayNotes(self) :
-            print (list(enumerate(self.notes)))
+    #def setState(self, state):
+    #    self.state = state
+
+    #def getState(self):
+    #    return self.state
+
+    def showTime(self):
+        print(self.openTime)
+
+    def addNotes(self, comment):
+        self.notes.append(comment)
+
+    def displayNotes(self):
+        print(list(enumerate(self.notes)))
