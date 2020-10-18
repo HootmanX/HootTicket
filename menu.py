a0bf483424b585e41287372ea908f0062cd11ead
Aaron <houghtalinga1@gmail.com>
2020-10-18 17:22:48 -0400

    Update menu.py
    **MAJOR UPGRADE** Moved more of the code to the HootDB.py, removed more code stored in memory so the program will work after closing and reopening and retain data.

--- menu.py
+++ menu.py
@@ -2,7 +2,6 @@ from openTicket import *
 import sys
 
 
-
 def login():
     # Password needs restructuring to call from hashed password files
     username = "root"
@@ -23,21 +22,22 @@ def login():
 
 def menu():
     finished = 0
-    ticketID = 0  # problematic on reopening console (possible save data to file/database?)
     tickets = []
     while finished == 0:
+        count = getLastId()
         print("\n" * 100)
         print("Hoot Ticket Management\n")
         print("Please select from one of the choices below")
-        selection = input("""
+        print("""
                             1. Open a new ticket.
                             2. Add comments to a ticket.
                             3. Show all open tickets.
-                            4. Close a ticket.
+                            4. View a ticket.
                             0. Exit
                             
-                            Number of tickets open: """ + str(ticketID) + """
-                            Selection: """)
+                            Number of tickets open: """, count, """
+Selection: """)
+        selection = input()
 
         # Open a new ticket
         if selection == "1":
@@ -45,11 +45,11 @@ def menu():
             while a == 0:
                 print("Please describe the request or incident")
                 n = input()
-                tickets.append(Ticket(ticketID, n))
-                print("Logged ticket request: ")
-                tickets[ticketID].displayIssue()
-                ticketID += 1
-                print("Would you like to open another ticket? (y/n)")
+                print("Successfully logged ticket request\n")
+                addTicket(n)
+                id = getLastId()
+                print("Ticket ID:", id)
+                print("\nWould you like to open another ticket? (y/n)")
                 n = input()
                 if n == "n" or n == "N" or n == "no" or n == "NO":
                     a = 1
@@ -60,20 +60,14 @@ def menu():
             n = input()
             # Display all open tickets so user can get ID #
             if n == "y" or n == "Y" or n == "yes" or n == "YES":
-                a = 0
-                for i in tickets:
-                    a += 1
-                    print("Ticket #" + str(a) + ":" + i.issue)
-                    print("\n\n")
+                showAll()
+
             print("Please enter ticket ID")
-            n = input()
-            m = int(n)
+            id = input()
             print("You have selected: " + n)
             print("Comment to add?")
-            com = input()
-            tickets[m].addNotes(com)
-            print("Displaying all comments on ticket")
-            tickets[m].displayNotes()
+            comment = input()
+            addComment(id, comment)
             print("Enter any key to return to the menu.")
             n = input()
 
@@ -82,13 +76,21 @@ def menu():
 
         # Display all open tickets
         elif selection == "3":
-            a = 0
-            for i in tickets:
-                a += 1
-                print("Ticket #" + str(a) + ":" + i.issue)
-                print("Ticket was opened on ")
-                i.showTime()
-                print("\n")
+            showAll()
+            print("Enter any key to return to the menu.")
+            n = input()
+
+        # View a ticket
+        elif selection == "4":
+            print("Would you like to view all tickets to get ID? (y/n)")
+            n = input()
+            # Display all open tickets so user can get ID #
+            if n == "y" or n == "Y" or n == "yes" or n == "YES":
+                showAll()
+
+            print("Please enter ticket ID")
+            n = input()
+            viewTicket(n)
 
             print("Enter any key to return to the menu.")
             n = input()
@@ -98,4 +100,5 @@ def menu():
         elif selection == "0":
             finished = 1
             sys.exit
+
 
